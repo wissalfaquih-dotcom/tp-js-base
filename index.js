e4c7a64b92977fc91b6841e05037fc5ea226eba2
Wissal FAQUIH <wissal.faquih@gmail.com>
2025-10-24 20:28:09 +0100

    TP JavaScript Moderne terminé

---
+++ index.js
@@ -0,0 +1,19 @@
+
+var x = 5;
+let y = 10;
+const z = 15;
+
+console.log("Avant réassignation :");
+console.log("x =", x);
+console.log("y =", y);
+console.log("z =", z);
+
+x = 20; 
+y = 30; 
+console.log("Après réassignation :");
+console.log("x =", x);
+console.log("y =", y);
+z =  50;
+
+console.log("z =", z);
+
